ef9dfd137611b5fa3871465b217610591f8358b0
Elli <elli.ludwin@gmail.com>
2021-10-21 19:40:25 -0400

    Completed 02 and passed the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,15 @@
 let string = "hey";
-let underScore = " ";
-let errorN = "Error";
+let errorN = "ERROR";
 
 const repeatString = function(string, num) {
     for (i = 0; i < num; i++){
-        if(num == 0){
-            return underScore;
-        }else if(Math.sign(num) == -1 || Math.sign(num) == -0){
-            return errorN;
-        }else {
             return string.repeat(num); 
-        }
+    }
+
+    if(num == 0){
+        return string.repeat(num);
+    }else if(Math.sign(num) == -1 || Math.sign(num) == -0){
+        return "ERROR";
     }
 };
 
