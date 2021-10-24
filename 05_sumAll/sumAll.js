045fa7437307d02d7fa91dfe3feb15b4027cb926
Elli <elli.ludwin@gmail.com>
2021-10-24 18:25:29 -0400

    Passed first test for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //create an array for elements between a and b;
+    const arrAB = [];
+    for (let i = a; i <= b; i++){
+        arrAB.push(i);
+    }
+    //sum all elements in the array;
+    let sumArr = 0;
+    for (const val of arrAB){
+        sumArr += val;
+    }
+    return sumArr;
 
 };
 
