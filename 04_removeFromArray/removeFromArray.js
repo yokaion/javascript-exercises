c317643229e0bd1417a6fcb8c105d7db2c4ac0c5
Elli <elli.ludwin@gmail.com>
2021-10-24 17:31:42 -0400

    Added a comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 
 const removeFromArray = function(...args) {
     const arr = args[0];
+    // filter() returns whatever elements that are not inluded in the args elements.
     return arr.filter(elem => !args.includes(elem));
 };
 
