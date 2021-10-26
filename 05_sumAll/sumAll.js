6c80ecbfe80375a05bb13c3b61e8d2dcc771ffe9
Elli <elli.ludwin@gmail.com>
2021-10-26 15:13:15 -0400

    06 passed the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,19 @@
 const sumAll = function(a, b) {
     //create an array for elements between a and b;
     const arr = [];
-    for (let i = a; i <= b; i++){
-        arr.push(i);
+    
+    if(isNaN(a)||isNaN(b)||
+        Math.sign(a)===-1||Math.sign(b)===-1||
+        typeof a === "string"||typeof b === "string"){
+        return "ERROR";
+    }else if(a < b){
+        for (let i = a; i <= b; i++){
+            arr.push(i);
+        }
+    }else if(a > b){
+        for (let i = b; i <= a; i++){
+            arr.push(i);
+        }
     }
     //sum all elements in the array;
     let sumArr = 0;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Check if year is divisible by 4 or 400;
+    if(year % 4 == 0 && year % 100 != 0 ||
+        year % 400 == 0){
+            return true;
+    }else {
+        return false;
+    }
 
 };
 
