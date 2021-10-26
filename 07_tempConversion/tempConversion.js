7651353167d4ccd475c3414e3adadcfbf868d29c
Elli <elli.ludwin@gmail.com>
2021-10-26 15:44:19 -0400

    07 passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(t_f) {
+  const t_c = (t_f - 32) * (5/9);
+  return Math.round(t_c * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(t_c) {
+  const t_f = t_c * (9/5) + 32;
+  return Math.round(t_f * 10)/10;
 };
 
 // Do not edit below this line
